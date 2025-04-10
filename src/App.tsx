a70afffaf7ee9099f1830dcf28b3d6471fab7c0b
Keiko Iwata <oumelab@pt.rdy.jp>
2025-04-10 16:00:20 +0900

    feat: add svg icons with vite-plugin-svgr

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
+import Refresh from "./assets/refresh.svg?react";
+import Play from "./assets/play.svg?react";
+
+
 export default function App() {
   return (
     <div className="flex flex-col gap-10 pt-16 pb-8 px-6">
@@ -16,8 +20,8 @@ export default function App() {
           </ul>
           <div className="py-8 grid place-content-center"><span className="text-7xl sm:text-8xl md:text-9xl font-semibold">25:00</span></div>
           <ul className="flex justify-center items-center gap-4">
-            <li><button className="size-12 bg-bg-muted rounded-full grid place-content-center text-2xl cursor-pointer">▶️</button></li>
-            <li><button className="size-12 bg-bg-muted rounded-full grid place-content-center text-2xl cursor-pointer">🔁</button></li>
+            <li><button className="size-12 bg-bg-muted rounded-full grid place-content-center text-2xl cursor-pointer"><Play /></button></li>
+            <li><button className="size-12 bg-bg-muted rounded-full grid place-content-center text-2xl cursor-pointer"><Refresh /></button></li>
           </ul>
           </div>
         </div>
